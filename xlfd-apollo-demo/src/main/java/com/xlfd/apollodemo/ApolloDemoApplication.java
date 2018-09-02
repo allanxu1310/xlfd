5a43b869531d6527f3fd24b2dfa64cd49199687b
Allan <allanxu1310@gmail.com>
2018-09-02 19:23:54 +0800

    调试

--- xlfd-apollo-demo/src/main/java/com/xlfd/apollodemo/ApolloDemoApplication.java
+++ xlfd-apollo-demo/src/main/java/com/xlfd/apollodemo/ApolloDemoApplication.java
@@ -1,16 +1,38 @@
 package com.xlfd.apollodemo;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableApolloConfig
 @EnableEurekaClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class ApolloDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApolloDemoApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+	@RequestMapping("/hi")
+	@HystrixCommand(fallbackMethod = "hiError")
+	public String home(@RequestParam String name) {
+		return "hi "+name+",i am apollo-demo from port:" +port;
+	}
+
+	public String hiError(String name) {
+		return "hi,"+name+",sorry,error!";
 	}
 }
--- xlfd-lottery/src/main/java/com/xlfd/lottery/LotteryApplication.java
+++ xlfd-lottery/src/main/java/com/xlfd/lottery/LotteryApplication.java
@@ -1,15 +1,39 @@
 package com.xlfd.lottery;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableFeignClients //开启Feign
 @SpringCloudApplication
+@EnableEurekaClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class LotteryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LotteryApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+	@RequestMapping("/hi")
+	@HystrixCommand(fallbackMethod = "hiError")
+	public String home(@RequestParam String name) {
+		return "hi "+name+",i am lottery from port:" +port;
+	}
+
+	public String hiError(String name) {
+		return "hi,"+name+",sorry,error!";
 	}
 }
